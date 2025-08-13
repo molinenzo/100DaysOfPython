2e4bc00db748d00668877375bfe967b1e8969169
Enzo Molina <enzodevmolina@gmail.com>
2025-08-12 23:27:24 -0300

    Project 1 - Band Name Generator
    This is the first project, very simple and basic, but we should never underestimate small beginnings. For now, it's an introduction to using strings, variables, and how to input and print information.

---
+++ band_name_generator.py
@@ -0,0 +1,12 @@
+print("\n======================================")
+print("  Welcome to the Band Name Generator!")
+print("======================================\n")
+
+city = input("What's the name of city you grew up in?\n")
+name = input("\nWhat's your pet's name?\n")
+
+print("\n-------------------------------------------")
+print("\nYour band name could be: " + city + " " + name)
+print("\n-------------------------------------------\n")
+
+input("\nPress Enter to exit...")
